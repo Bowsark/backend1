421b88b06fca6aa774dc8f58efcdf1aa28581682
Nneoma Onuabuchi <onuabuchij@yahoo.com>
2024-11-14 15:48:43 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const bcryptjs = require('bcryptjs');
-const User = require('../models/User.js');
+const User = require('../models/user');
 
 const router = express.Router();
 
